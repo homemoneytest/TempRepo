066729c706ec885ff74bbe641e766ee330837d55
Igor Andreichyn <vidmak.case+git@gmail.com>
2012-05-29 01:00:30 +0300

    Button Rename

--- HomemoneyFakeApp/Form1.Designer.cs
+++ HomemoneyFakeApp/Form1.Designer.cs
@@ -37,7 +37,7 @@
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(266, 240);
 			this.button1.TabIndex = 0;
-			this.button1.Text = "Start";
+			this.button1.Text = "ButtonFeature";
 			this.button1.UseVisualStyleBackColor = true;
 			// 
 			// Form1
